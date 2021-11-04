b97c0cae43a1de7314612dd715affd2907d23887
MissStrange <esanchezloarte@gmail.com>
2021-11-05 00:41:53 +0100

    Modificacion Typewriter Script
    Se ha modificado el Script TypewriterEffect para generar pausas con los signos de puntuación

--- My project/Assets/Scripts/Dialogos/TypewriterEffect.cs
+++ My project/Assets/Scripts/Dialogos/TypewriterEffect.cs
@@ -6,6 +6,13 @@ using TMPro;
 public class TypewriterEffect : MonoBehaviour
 {
     [SerializeField] private float typewriterSpeed = 40;
+
+    private readonly Dictionary<HashSet<char>, float> punctuations = new Dictionary<HashSet<char>, float>()
+    {
+        {new HashSet<char>(){'.', '!', '?'}, 0.6f },
+        {new HashSet<char>(){',', ';', ':'}, 0.3f },
+    };
+
     public Coroutine Run(string textToType, TMP_Text textLabel)
     {
         return StartCoroutine(TypeText(textToType, textLabel));
@@ -21,17 +28,44 @@ public class TypewriterEffect : MonoBehaviour
 
         while (charIndex < textToType.Length)
         {
+            int lastCharIndex = charIndex;
+
             //se incrementa a lo largo del tiempo
             t += Time.deltaTime * typewriterSpeed;
+
             //almacena el valor redondeado de la variable t
             charIndex = Mathf.FloorToInt(t);
             //nos asegura que el valor de charIndex no es mayor que textToType
             charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
 
-            textLabel.text = textToType.Substring(0, charIndex);
+            for (int i = lastCharIndex; i< charIndex; i++)
+            {
+                bool isLast = i >= textToType.Length - 1;
+                textLabel.text = textToType.Substring(0, i + 1);
+                if (IsPunctuation(textToType[i], out float waitTime)&& !isLast && !IsPunctuation(textToType[i + 1], out _))
+                {
+                    yield return new WaitForSeconds(waitTime);
+                }
+            }
+
+            
             yield return null;
         }
 
         textLabel.text = textToType;
+    }
+
+    private bool IsPunctuation(char character, out float waitTime)
+    {
+        foreach(KeyValuePair<HashSet<char>, float> punctuationCategory in punctuations)
+        {
+            if (punctuationCategory.Key.Contains(character))
+            {
+                waitTime = punctuationCategory.Value;
+                return true;
+            }
+        }
+        waitTime = default;
+        return false;
     }
 }
